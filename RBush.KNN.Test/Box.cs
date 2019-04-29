4912da29f8b780ad528e96c04cf5d7d960828336
Stuart Turner <stuart@turner-isageek.com>
2019-04-29 16:45:22 -0500

    Implement RBush.KNN

---
+++ RBush.KNN.Test/Box.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RBush.KNN.Test
+{
+	public class Box : ISpatialData, IComparable<Box>
+	{
+		private Envelope _envelope;
+		public ref readonly Envelope Envelope => ref _envelope;
+
+		public double DistanceTo(double x, double y) =>
+			DistanceTo(Envelope, x, y);
+
+		private static double DistanceTo(in Envelope envelope, double x, double y)
+		{
+			var dX = AxisDistance(x, envelope.MinX, envelope.MaxX);
+			var dY = AxisDistance(y, envelope.MinY, envelope.MaxY);
+			return Math.Sqrt((dX * dX) + (dY * dY));
+		}
+
+		private static double AxisDistance(double k, double min, double max) =>
+			k < min ? min - k :
+			k > max ? k - max :
+			0;
+
+		public int Version { get; set; } = 0;
+
+		public int CompareTo(Box other)
+		{
+			if (this.Envelope.MinX != other.Envelope.MinX)
+				return this.Envelope.MinX.CompareTo(other.Envelope.MinX);
+			if (this.Envelope.MinY != other.Envelope.MinY)
+				return this.Envelope.MinY.CompareTo(other.Envelope.MinY);
+			if (this.Envelope.MaxX != other.Envelope.MaxX)
+				return this.Envelope.MaxX.CompareTo(other.Envelope.MaxX);
+			if (this.Envelope.MaxY != other.Envelope.MaxY)
+				return this.Envelope.MaxY.CompareTo(other.Envelope.MaxY);
+			return 0;
+		}
+
+		public static Box[] CreateBoxes(double[,] data)
+		{
+			return Enumerable.Range(0, data.GetLength(0))
+				.Select(i => new Box
+				{
+					_envelope = new Envelope
+					(
+						minX: data[i, 0],
+						minY: data[i, 1],
+						maxX: data[i, 2],
+						maxY: data[i, 3]
+					),
+					Version = i + 1,
+				})
+				.ToArray();
+		}
+
+		public static Box CreateBox(double[] data) =>
+			new Box
+			{
+				_envelope = new Envelope(
+					minX: data[0],
+					minY: data[1],
+					maxX: data[2],
+					maxY: data[3])
+			};
+	}
+}
---
+++ RBush.KNN/RBush.Knn.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RBush
+{
+	public static class RBushExtensions
+	{
+		public static IReadOnlyList<T> Knn<T>(
+			this RBush<T> tree,
+			int k,
+			double x,
+			double y,
+			double? maxDistance = null,
+			Func<T, bool> predicate = null)
+			where T : ISpatialData
+		{
+			var items = maxDistance == null
+				? tree.Search()
+				: tree.Search(
+					new Envelope(
+						minX: x - maxDistance.Value,
+						minY: y - maxDistance.Value,
+						maxX: x + maxDistance.Value,
+						maxY: y + maxDistance.Value));
+
+			IEnumerable<(T i, double dist)> distances = items
+				.Select(i => (i, dist: i.Envelope.DistanceTo(x, y)))
+				.OrderBy(i => i.dist);
+
+			if (maxDistance.HasValue)
+				distances = distances.TakeWhile(i => i.dist <= maxDistance.Value);
+
+			if (predicate != null)
+				distances = distances.Where(i => predicate(i.i));
+
+			if (k > 0)
+				distances = distances.Take(k);
+
+			return distances
+				.Select(i => i.i)
+				.ToList();
+		}
+
+		private static double DistanceTo(this in Envelope envelope, double x, double y)
+		{
+			var dX = AxisDistance(x, envelope.MinX, envelope.MaxX);
+			var dY = AxisDistance(y, envelope.MinY, envelope.MaxY);
+			return Math.Sqrt((dX * dX) + (dY * dY));
+		}
+
+		private static double AxisDistance(double k, double min, double max) =>
+			k < min ? min - k :
+			k > max ? k - max :
+			0;
+	}
+}
